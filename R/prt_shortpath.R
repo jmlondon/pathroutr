0880f15149d53406f1ffcef925d426852cca43cc
Josh.London <josh.london@noaa.gov>
2020-11-24 17:49:21 -0800

    CALCULATE prt_shortpath() SAFELY
    stplanr::sum_network_routes currently returns an error if start/end nodes are the same. this patch handles that via purrr::safely and creates a degenerate linestring when the nodes are determined to be the same.

--- R/prt_shortpath.R
+++ R/prt_shortpath.R
@@ -8,15 +8,33 @@
 #'
 prt_shortpath <- function(segs_tbl, vis_graph) {
 
-  start <- segs_tbl$start_node
-  end <- segs_tbl$end_node
+  shortpath <- function(sln, start, end) {
+    stplanr::sum_network_routes(sln, start, end)
+  }
 
-  p <- stplanr::sum_network_routes(sln = vis_graph,
-                                   start = start,
-                                   end = end)
-  segs_tbl <- cbind(segs_tbl, p) %>%
+  safe_shortpath <- purrr::safely(shortpath)
+
+  res_tbl <- vector(mode = "list", length = nrow(segs_tbl))
+  for (i in 1:nrow(segs_tbl)) {
+    res_tbl[[i]] <-
+      safe_shortpath(vis_graph, segs_tbl$start_node[i], segs_tbl$end_node[i])
+
+    if(segs_tbl$start_node[i] == segs_tbl$end_node[i]) {
+      node_pt <- stplanr::sln2points(vis_graph)[i,]
+      g <- rbind(node_pt, node_pt) %>% st_combine() %>% st_cast('LINESTRING')
+      l_sf <- tibble(geometry = g, ID = NA, sum_length = NA, pathfound = FALSE) %>%
+        st_sf(sf_column_name = "geometry")
+      res_tbl[[i]]$result <- l_sf
+    }
+
+    }
+
+  res_tbl <- res_tbl %>% purrr::map_dfr("result")
+
+  segs_tbl <- cbind(segs_tbl, res_tbl) %>%
     dplyr::rowwise() %>%
     dplyr::mutate(geometry = prt_extend_line(geometry, start_pt, end_pt)) %>%
     dplyr::select(sid:geometry)
+
   return(segs_tbl)
 }
