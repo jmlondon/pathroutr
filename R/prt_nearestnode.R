ae1141232d00e3e859f6e1f3b229fb5d387413a6
Josh.London <josh.london@noaa.gov>
2020-12-01 21:29:22 -0800

    SPEED UP FINDING NEAREST NODE
    switched to use of {nabor} and knn() function for finding the start and end nodes. much much faster.

--- R/prt_nearestnode.R
+++ R/prt_nearestnode.R
@@ -3,18 +3,26 @@
 #'
 #' @param segs_tbl output from get_barrier_segments
 #' @param vis_graph output from prt_visgraph
-#' @param maxdist max search distance for locating the nearest node
 #'
 #' @return segs_tbl with updated columns for nearest start and end nodes
 #' @export
 
-prt_nearestnode <- function(segs_tbl, vis_graph, maxdist = 15000) {
-  segs_tbl <- segs_tbl %>%
+prt_nearestnode <- function(segs_tbl, vis_graph) {
+  # segs_tbl <- segs_tbl %>%
+  #   dplyr::mutate(
+  #     start_node = stplanr::find_network_nodes(vis_graph, sf::st_coordinates(.$start_pt),
+  #                                              maxdist = maxdist),
+  #     end_node = stplanr::find_network_nodes(vis_graph, sf::st_coordinates(.$end_pt),
+  #                                            maxdist = maxdist)
+  #   )
+  segs_tbl <- segs_tbl %>% ungroup() %>%
     dplyr::mutate(
-      start_node = stplanr::find_network_nodes(vis_graph, sf::st_coordinates(.$start_pt),
-                                               maxdist = maxdist),
-      end_node = stplanr::find_network_nodes(vis_graph, sf::st_coordinates(.$end_pt),
-                                             maxdist = maxdist)
+      start_node = nabor::knn(st_coordinates(stplanr::sln2points(vis_graph)),
+                              sf::st_coordinates(.$start_pt),
+                              k = 1)$nn.idx[,1],
+      end_node = nabor::knn(st_coordinates(stplanr::sln2points(vis_graph)),
+                              sf::st_coordinates(.$end_pt),
+                              k = 1)$nn.idx[,1],
     )
   return(segs_tbl)
 }
