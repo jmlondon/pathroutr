5ce18b34ec7da7490ade4942f6845a2f5912ea57
Josh.London <josh.london@noaa.gov>
2020-12-01 17:34:57 -0800

    REMOVE INCLUDED vis_graph DATA

--- R/vis_graph.R
+++
@@ -1,8 +0,0 @@
-#' visual graph for Alaska coast region
-#'
-#' A visual graph built with `prt_visgraph()` and the Alaska coastal region used
-#' by the harbor seal example. Included to save time during vignette build.
-#'
-#' @format sfNetwork
-#'
-"vis_graph"
