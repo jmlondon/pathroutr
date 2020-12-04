db20481b524fc194bd62504177694a0a458ae6fa
Josh.London <josh.london@noaa.gov>
2020-12-03 21:23:17 -0800

    MORE ROBUST EXTENSION OF LINE
    casting to points, combine, and cast to LINESTRING seemed to be the most robust solution.

--- R/prt_extend_line.R
+++ R/prt_extend_line.R
@@ -12,5 +12,7 @@ prt_extend_line <- function(l, start_pt, end_pt) {
   p2 <- sf::st_line_sample(l, sample = 1) %>% sf::st_cast('POINT')
   l1 <- c(start_pt, p1) %>% sf::st_combine() %>% sf::st_cast('LINESTRING')
   l2 <- c(p2, end_pt) %>% sf::st_combine() %>% sf::st_cast('LINESTRING')
-  l <- c(l1,l,l2) %>% sf::st_combine() %>% sf::st_line_merge()
+  l <- c(l1,l,l2) %>% sf::st_cast('POINT') %>%
+    st_combine() %>%
+    st_cast('LINESTRING')
 }
